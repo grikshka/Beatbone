59743bb4a2862111474f10cf811535e8c858be6f
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2018-12-02 12:07:45 +0100

    Create MusicSearcher.java
    Creating MusicSearcher class in util package inside BLL layer and implementing searchSongs method which is taking list of songs and filter and returninng list of all songs that fits the filter

---
+++ MyTunes/src/mytunes/bll/util/MusicSearcher.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes.bll.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import mytunes.be.Song;
+
+/**
+ *
+ * @author Acer
+ */
+public class MusicSearcher {
+    
+    public static List<Song> searchSongs(List<Song> allSongs, String filter)
+    {
+        filter = filter.toLowerCase();
+        List<Song> filteredSongs = new ArrayList();
+        for(Song s : allSongs)
+        {
+            if(filter.length() <= s.getTitle().length() && filter.equals(s.getTitle().toLowerCase().substring(0, filter.length())))
+            {
+                filteredSongs.add(s);
+            }
+            else if(filter.length() <= s.getArtist().length() && filter.equals(s.getArtist().toLowerCase().substring(0, filter.length())))
+            {
+                filteredSongs.add(s);
+            }
+        }
+        return filteredSongs;
+    }
+    
+}

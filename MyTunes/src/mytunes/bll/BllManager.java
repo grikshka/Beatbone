42b0fb7941491c5b619ac522c2011c8b61552f2e
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2018-12-03 13:49:39 +0100

    Create BllManager.java
    Creating BllManager which is implementing IBllFacade and implementing createSong method

---
+++ MyTunes/src/mytunes/bll/BllManager.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes.bll;
+
+import java.util.List;
+import mytunes.be.Playlist;
+import mytunes.be.Song;
+import mytunes.dal.DalController;
+import mytunes.bll.IBllFacade;
+import mytunes.dal.IDalFacade;
+
+/**
+ *
+ * @author Acer
+ */
+public class BllManager implements IBllFacade{
+    
+    private IDalFacade dalController;
+    
+    public BllManager()
+    {
+        dalController = new DalController();
+    }
+
+    @Override
+    public Song createSong(String title, String artist, String genre, String path, int time) {
+        return dalController.createSong(title, artist, genre, path, time);
+    }
+
+    @Override
+    public Song updateSong(Song song, String title, String artist, String genre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteSong(Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Song> getAllSongs() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Playlist createPlaylist(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Playlist updatePlaylist(Playlist playlist, String newName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletePlaylist(Playlist playlist) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Playlist> getAllPlaylists() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Playlist moveSongUpOnPlaylist(Playlist playlist, Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Playlist moveSongDownOnPlaylist(Playlist playlist, Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteSongFromPlaylist(Playlist playlist, Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Playlist addSongToPlaylist(Playlist playlist, Song song) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
